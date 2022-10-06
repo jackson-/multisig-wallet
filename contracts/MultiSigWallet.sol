c45fa53e974973e111210587dea0b6c219040f44
Remix Workspace <>
2022-10-06 16:28:25 -0400

    initial fields before constructor

---
+++ contracts/MultiSigWallet.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint indexed txId);
+    event Approve(address indexed owner, uint indexed txId);
+    event Revoke(address indexed owner, uint indexed txId);
+    event Execute(uint indexed txId);
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+    }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public reuqired;
+
+    Transaction[] public transactions;
+    mapping(uint => mapping(address => bool)) public approved;
+
+}
