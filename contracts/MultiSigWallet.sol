0ec03aa03edc8baeed7fb28bd935e3ea71914960
Remix Workspace <>
2022-10-06 22:30:44 -0400

    constructor, submit, and approval functions again

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -22,4 +22,64 @@ contract MultiSigWallet {
     Transaction[] public transactions;
     mapping(uint => mapping(address => bool)) public approved;
 
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint _txId) {
+        require(transactions.length < _txId, "tx does not exist");
+        _;
+    }
+    modifier notApproved(uint _txId) {
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint required){
+        require(_owners.length > 0, "owners required");
+        require(
+            required > 0 && required <= owners.length,
+            "invalid required number of owners"
+        );
+        for(uint i; i < owners.length; i++) {
+            address owner = _owners[i];
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner is not unique");
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function submit(address _to, uint _value, bytes calldata data) 
+        external
+        onlyOwner
+    {
+        transactions.push(Transaction({
+            to: _to,
+            value: _value,
+            data: data,
+            executed: false
+        }));
+        emit Submit(transactions.length - 1);
+    }
+
+    function approve(uint _txId) 
+        external
+        onlyOwner
+        txExists(_txId)
+        notApproved(_txId)
+        notExecuted(_txId)
+    {
+        approved[_txId][msg.sender] = true;
+        emit Approve(msg.sender, _txId);
+    }
 }
